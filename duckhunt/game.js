6a13bd020f00b39a066c22fc3c0a686f5db18275
Adam Greenspan <agreenspan24@gmail.com>
2015-10-14 00:02:02 -0400

    Comments to script

--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,19 +1,22 @@
-function init(){
+function init() {
+	//sets background to variable "img"
 	var img = new Image(); 
 	img.src = "duckhunt-background.gif";
 
+	// sets various sheet to variable elem
 	var elem = new Image();
-	elem.src = "duckhunt_various_sheet.png"
+	elem.src = "duckhunt_various_sheet.png";
 
+	//sets up canvas using id in index.html
 	var canvas = document.getElementById("game_canvas");
 	var ctx = canvas.getContext("2d");
 
+	// slices in background
 	ctx.drawImage(img, 0, 0, 256, 240, 0, 0, 256, 240);
 
+	// slices in two birds
 	ctx.drawImage(elem, 0, 115, 40, 40, 90, 40, 40, 40);
 	ctx.drawImage(elem, 170, 155, 40, 40, 175, 75, 40, 40);
-
-
 
 }
 
