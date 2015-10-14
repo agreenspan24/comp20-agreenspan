4446c03028c8282c419beea4a097fbac2846c0e7
Adam Greenspan <agreenspan24@gmail.com>
2015-10-13 23:56:37 -0400

    Adding README

---
+++ duckhunt/README.md
@@ -0,0 +1,5 @@
+README.md
+Adam Greenspan
+10/13/15
+
+In this lab, I have successfully implemented a background and two ducks using a canvas. I did not animate the birds. I worked for approximately 2 hours on this project, and worked alone. 
--- duckhunt/game.js
+++ duckhunt/game.js
@@ -1,1 +1,24 @@
-// Replace this with your code...
+function init(){
+	var img = new Image(); 
+	img.src = "duckhunt-background.gif";
+
+	var elem = new Image();
+	elem.src = "duckhunt_various_sheet.png"
+
+	var canvas = document.getElementById("game_canvas");
+	var ctx = canvas.getContext("2d");
+
+	ctx.drawImage(img, 0, 0, 256, 240, 0, 0, 256, 240);
+
+	ctx.drawImage(elem, 0, 115, 40, 40, 90, 40, 40, 40);
+	ctx.drawImage(elem, 170, 155, 40, 40, 175, 75, 40, 40);
+
+
+
+}
+
+
+
+
+
+
