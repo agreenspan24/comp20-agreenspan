c2bf001055ab358d51e1d00241509ae09bfe980f
Adam Greenspan <agreenspan24@gmail.com>
2015-10-23 13:20:43 -0400

    Debugging

--- messages/lab.js
+++ messages/lab.js
@@ -5,11 +5,15 @@ function parse() {
 
 	request.onreadystatechange = function() {
 		if (request.readyState == 4) {
+			console.log("step1");
 			data = request.responseText;
+			console.log("step2");
 			msg = JSON.parse(data);
+			console.log("step3");
 			elem = document.getElementById("messages");
+			console.log("step4");
 			for (var i = 0; i < msg.length; i++){
-				console.log("i: " + i);
+				console.log("i: " + (i+5));
 				elem.innerHTML = "<p>" + msg[i].content + " " +
 					msg[i].username + "</p>";
 			}
