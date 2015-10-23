3b20a01ed579d0e1315effb543af5007a27e0e0c
Adam Greenspan <agreenspan24@gmail.com>
2015-10-23 14:20:37 -0400

    final formatting

--- messages/lab.js
+++ messages/lab.js
@@ -1,18 +1,23 @@
 var request = new XMLHttpRequest();
 
 function parse() {
+	//set up http request
 	request.open("GET", "http://messagehub.herokuapp.com/messages.json", true);
 
+	//set up callback function
 	request.onreadystatechange = function() {
 		if (request.readyState == 4) {
 			data = request.responseText;
 			msg = JSON.parse(data);
 			elem = document.getElementById("messages");
+			// add to id
 			for (var i = 0; i < msg.length; i++){
 				elem.innerHTML += "<p>" + msg[i].content + " " +
 					msg[i].username + "</p>";
 			};
 		}
 	}
+
+	//trigger http request
 	request.send(null);
 }
