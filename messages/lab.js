54a112b13a606833550c5bb1b9e545f6354a5b68
Adam Greenspan <agreenspan24@gmail.com>
2015-10-23 13:07:44 -0400

    Starting Lab 6

---
+++ messages/lab.js
@@ -0,0 +1,18 @@
+var request = new XMLHttpRequest();
+
+function parse() {
+	request.open("GET", "data.json", true);
+
+	request.onreadystatechange = function() {
+		if (request.readyState == 4) {
+			data = request.responseText;
+			msg = JSON.parse(data);
+			elem = document.getElementById("messages");
+			for (var i = 0; i < msg.length; i++){
+				elem.innerHTML = "<p>" + msg[i].content + " " +
+					msg[i].username;
+			}
+		}
+	}
+	request.send(null);
+}
